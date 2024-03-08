cc8deb6904850220afdf44399b34eff8fb014273
Youcef Bouterfass <youcef@callalawyer.fr>
2024-03-08 21:57:08 +0100

    almost finished

--- popup.js
+++ popup.js
@@ -3,28 +3,47 @@ document.getElementById('wordForm').addEventListener('submit', function (event) 
     event.preventDefault();
 
 
+    const container = document.getElementById('message');
+    const message = document.createElement('p');
+    message.style.fontWeight = "bold";
+
     const json = document.getElementById('json').value;
-    let data = JSON.parse(json);
-    console.table(data['sentences']);
+    let data = json ? JSON.parse(json) : null;
 
-    fetch('http://localhost:3006/api/v1/vocab/add-word', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            japaneseWord,
-            wordTranslation,
-            romaji,
-            kana,
-            categories,
-            level
+    if (data) {
+        fetch('http://localhost:3006/api/v1/vocab/add-word', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                data
+            })
         })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success: ', data)
-        })
-        .catch(error => console.log('Error: ', error))
-
+            .then(response => response.json())
+            .then(data => {
+                message.textContent = "Mot ajouté avec succés!";
+                message.style.color = 'green';
+                console.log('Success: ', data);
+            })
+            .catch(error => {
+                message.textContent = "Une erreur s'est produite.";
+                message.style.color = 'red';
+                console.log('Error: ', error);
+            })
+            .finally(() => {
+                container.appendChild(message);
+                setTimeout(() => {
+                    container.removeChild(message);
+                }, 3000);
+            })
+    } else {
+        message.textContent = "Veuillez ajouter un mot.";
+        message.style.color = 'orange';
+        container.appendChild(message);
+        setTimeout(() => {
+            container.removeChild(message);
+        }, 3000);
+    }
 });
+
